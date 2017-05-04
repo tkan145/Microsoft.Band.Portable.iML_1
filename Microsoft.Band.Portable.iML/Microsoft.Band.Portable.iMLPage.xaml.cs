0db1f422cc885ea38ec1b639eee0d7c495c5ce6a
An Tran <tkan145@gmail.com>
2017-05-05 02:33:11 +1000

    Init

---
+++ Microsoft.Band.Portable.iML/Microsoft.Band.Portable.iMLPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Microsoft.Band.Portable.iML
+{
+	public partial class Microsoft_Band_Portable_iMLPage : ContentPage
+	{
+		public Microsoft_Band_Portable_iMLPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
