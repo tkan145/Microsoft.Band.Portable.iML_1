a174e0acacc9a9ba446eca753eecb9263b09a19b
An Tran <tkan145@gmail.com>
2017-05-12 00:57:51 +1000

    Read/Write

---
+++ Microsoft.Band.Portable.iML/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MvvmHelpers;
+
+namespace Microsoft.Band.Portable.iML
+{
+	public class DashboardViewModel : BaseViewModel
+	{
+		private readonly IFileService _fileService;
+		private bool _isLoadingData;
+		public DashboardViewModel()
+		{
+		}
+
+		public bool IsLoadingData
+		{
+			get { return _isLoadingData; }
+			set
+			{
+				if (value == _isLoadingData) return;
+				_isLoadingData = value;
+				OnPropertyChanged(() => IsLoadingData);
+			}
+		}
+
+		public ObservableRangeCollection<Model> Models { get; set; }
+
+		public ICommand UpdateCompaniesCommand { get; set; }
+
+		public async Task Init()
+		{
+			await UpdateModelsList();
+		}
+
+		private async void UpdateModels()
+		{
+			IsLoadingData = true;
+
+			await _fileService.UpdateItemsAsync();
+			await UpdateModelsList();
+
+			IsLoadingData = false;
+		}
+
+		private async Task UpdateModelsList()
+		{
+			var models = await _fileService.GetItemsAsync();
+
+			Models.Clear();
+
+			foreach (var each in models)
+			{
+				Models.Add(each);
+			}
+		}
+	}
+}
--- Microsoft.Band.Portable.iML/Views/Pages/Home/DashboardPage.xaml.cs
+++ Microsoft.Band.Portable.iML/Views/Pages/Home/DashboardPage.xaml.cs
@@ -5,11 +5,21 @@ using Xamarin.Forms;
 
 namespace Microsoft.Band.Portable.iML.Views
 {
-    public partial class DashboardPage : ContentPage
-    {
-        public DashboardPage()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class DashboardPage : ContentPage
+	{
+		private readonly DashboardViewModel _viewModel;
+		public DashboardPage()
+		{
+			InitializeComponent();
+
+			//_viewModel = App.Locator.Main;
+			BindingContext = _viewModel;
+		}
+
+		protected override async void OnAppearing()
+		{
+			await _viewModel.Init();
+			base.OnAppearing();
+		}
+	}
 }
